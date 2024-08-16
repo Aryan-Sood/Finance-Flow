b8ba1ec8b40841198d2f4656fa54c0dd057d6655
Aryan-Sood <aryansood12@gmail.com>
2024-08-17 02:28:53 +0530

    Init project

---
+++ screens/ManageExpense.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function ManageExpense() {
+  return (
+    <View>
+      <Text>Manage Expense</Text>
+    </View>
+  )
+}
---
+++ screens/RecentExpenses.js
@@ -0,0 +1,10 @@
+import { View, Text } from 'react-native'
+import React from 'react'
+
+export default function RecentExpenses() {
+  return (
+    <View>
+      <Text>Recent Expenses</Text>
+    </View>
+  )
+}
